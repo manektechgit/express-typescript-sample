7b445cfd085e4db247ad772a6c4598eba4c0ef0d
Manektech Git <git@manektech.com>
2022-06-09 12:59:37 +0530

    Initial Commit

---
+++ users/users.controller.ts
@@ -0,0 +1,67 @@
+import { NextFunction, Request, Response } from 'express';
+import { CreateUserDto } from '@dtos/users.dto';
+import { User } from '@interfaces/users.interface';
+import userService from '@services/users.service';
+import { buildResponse } from '@/utils/util';
+import { statusCodes } from '@utils/constants';
+
+class UsersController {
+  public userService = new userService();
+
+  public getUsers = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const findAllUsersData: User[] = await this.userService.findAllUser();
+
+      res.status(statusCodes.OK).json(buildResponse(statusCodes.OK, true, 'findAll', findAllUsersData));
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  public getUserById = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const userId: string = req.params.id;
+      const findOneUserData: User = await this.userService.findUserById(userId);
+
+      res.status(statusCodes.OK).json(buildResponse(statusCodes.OK, true, 'findOne', findOneUserData));
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  public createUser = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const userData: CreateUserDto = req.body;
+      const createUserData: User = await this.userService.createUser(userData);
+
+      res.status(statusCodes.CREATED).json(buildResponse(statusCodes.CREATED, true, 'created', createUserData));
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  public updateUser = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const userId: string = req.params.id;
+      const userData: CreateUserDto = req.body;
+      const updateUserData: User = await this.userService.updateUser(userId, userData);
+
+      res.status(statusCodes.OK).json(buildResponse(statusCodes.OK, true, 'updated', updateUserData));
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  public deleteUser = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const userId: string = req.params.id;
+      const deleteUserData: User = await this.userService.deleteUser(userId);
+
+      res.status(statusCodes.OK).json(buildResponse(statusCodes.OK, true, 'deleted', deleteUserData));
+    } catch (error) {
+      next(error);
+    }
+  };
+}
+
+export default UsersController;
